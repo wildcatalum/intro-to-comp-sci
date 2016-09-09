f908e5a5f7db3497821e91b92821c602061171bb
wildcatalum <efrenibarra@email.arizona.edu>
2016-09-08 20:50:19 -0700

    complete code of robot

--- asciiart.py
+++ asciiart.py
@@ -1,8 +1,36 @@
 # Efren Ibarra, CSC 110, Fall 2016, Section 1B
 # programming assignment #2A, 09/03/16
-
 def main():
-
+        antenna()
+        top()
+        gap()
+        eyes()
+        gap()
+        mouth()
+        gap()
+        top()
+        
 def top():
 	for i in range(1, 6):
-		print
+		print("__", end="")
+	print()
+
+def eyes():
+        print("|+++  +++|")
+        for i in range (1, 2):
+                print("|+ +  + +|")
+        print("|+++  +++|")
+
+def gap():
+        for i in range (1,2):
+                print("|        |")
+
+def mouth():
+        print ("| |||||| |")
+
+def antenna():
+        print("    MR     ")
+        for i in range(1, 5):
+                print("    **    ")
+        
+main()
